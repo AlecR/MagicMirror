1095662e68c8a14d643f31cd4bc46bdb8dd8f0ae
Alec Rodgers <alecr1997@gmail.com>
2018-07-01 13:39:23 -0400

    Remove leftover React-dnd files

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MagicMirror from './MagicMirror';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers'
-
-const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <MagicMirror />
-  </Provider>
+  <MagicMirror />
 , document.getElementById('root'));
